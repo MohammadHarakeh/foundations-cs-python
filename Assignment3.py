1d1f1650406be2fa6f8dac50c7db0eb518d9879d
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-10-26 10:43:41 +0300

    Assignment 1

--- Assignment3.py
+++ Assignment3.py
@@ -1,3 +1,19 @@
-name = str(input("What is your name? "))
-print(f"Hello {name} what would you like to do? ")
+def menu():
+    print("1. Add Matrices")
+    print("2. Check Rotation")
+    print("3. Invert Dictionary")
+    print("4. Convert Matrix to Dictionary")
+    print("5. Check Palindrome")
+    print("6. Search for an Element & Merge Sort")
+    print("7. Exit")
 
+
+def main():
+    name = str(input("What is your name? "))
+    print(f"Hello {name} what would you like to do? ")
+    print("")
+
+    menu()
+
+
+main()
