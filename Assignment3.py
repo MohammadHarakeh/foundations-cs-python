f24a0ddb7556bdddbe6c67138605457d2074f08e
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-10-29 13:34:15 +0200

    part 6 finished part of it will continue and do some more research

--- Assignment3.py
+++ Assignment3.py
@@ -169,6 +169,26 @@ def palindromeCheck(s):
 
 # palindromeCheck O(N)
 
+
+def mergeSort(s):
+    if len(s) > 1:
+        mid = len(s) // 2
+        left = s[:mid]
+        right = s[mid:]
+
+        mergeSort(left)
+        mergeSort(right)
+
+        i, j, k = 0, 0, 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                s[k] = left[i]
+                i += 1
+            else:
+                s[k] = right[j]
+                j += 1
+            k += 1
+
 def main():
     name = str(input("What is your name? "))
     print(f"Hello {name} what would you like to do? ")
