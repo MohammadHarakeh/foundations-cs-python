d0b7015b16d260f3085ea1efa404f1b2f962dc8b
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-11-09 19:44:59 +0200

    added the openTab() function and added comments to it

--- Midterm.py
+++ Midterm.py
@@ -1,4 +1,8 @@
-#This will display the menu that the user can choose from
+# This will display the menu that the user can choose from
+
+tab_storage = {}
+
+
 def menu():
     print("1. Open Tab")
     print("2. Close Tab")
@@ -11,4 +15,32 @@ def menu():
     print("9. Exit")
     print()
 
-menu()
+
+# This will ask the user for title and url that will be stored in tab storage that is outside the function so that the items won't get deleted
+def openTab():
+    title = input("Enter the title of the website: ")
+    url = input("Enter the URL of the website: ")
+    # if the title (key) is in tab storage then im adding another url (value) to it else I add one
+    if title in tab_storage:
+        tab_storage[title].append(url)
+    else:
+        tab_storage[title] = [url]
+    print(tab_storage)
+    print()
+
+
+def main():
+    name = input("Enter your name: ")
+    print(f"Hello {name} what would you like to do: ")
+    print()
+
+    while True:
+        menu()
+        choice = input("Enter your choice from 1 -> 9: ")
+        print()
+
+        if choice == "1":
+            openTab()
+
+
+main()
