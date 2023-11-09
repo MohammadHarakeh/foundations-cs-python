d47a8ccfbc801d6dfc1c5d225635f74f601cd380
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-11-09 19:15:54 +0200

    added the menu that the user can pick from

---
+++ Midterm.py
@@ -0,0 +1,14 @@
+#This will display the menu that the user can choose from
+def menu():
+    print("1. Open Tab")
+    print("2. Close Tab")
+    print("3. Switch Tab")
+    print("4. Display All Tabs")
+    print("5. Open Nested Tabs")
+    print("6. Clear All Tabs")
+    print("7. Save Tabs")
+    print("8. Import Tabs")
+    print("9. Exit")
+    print()
+
+menu()
