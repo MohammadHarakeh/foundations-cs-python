4a3a6e1718fe2885fb3f34f10e1a2c9792bb0534
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-11-12 01:29:45 +0200

    added more comments and fixed print statements to make it more user-friendly

--- Midterm.py
+++ Midterm.py
@@ -34,7 +34,7 @@ def openTab():
         title = input("Enter the title of the website: ")
         url = input("Enter the URL of the website: ")
 
-# This is used to make sure that the url will start will https
+        # This is used to make sure that the url will start will https
         if url.startswith("https://"):
             tab_dic["title"] = title
             tab_dic["url"] = url
@@ -42,13 +42,19 @@ def openTab():
         else:
             print("Invalid URL. Make sure that the URL starts with https://")
     open_tabs.append(tab_dic)
-    print(open_tabs)
-    print()
+    print(f"\nNumber of open tabs are: {len(open_tabs)}")
+    print(f"{open_tabs}\n")
 
 
 # This function will remove a tab after specifying the index
 def closeTab():
-    user_input = input("Enter the index of the tab you want to close: ")
+    user_input = None
+    if len(open_tabs) == 1:
+        user_input = input("There is only one tab open at index 0\ntype 0 to remove it: ")
+    else:
+        user_input = input(
+            f"there are {len(open_tabs)} open tabs what index you want to remove from 0 -> {len(open_tabs) - 1}:  ")
+
     # this will make sure that the entered is a digit if not it will then remove the last index
     if user_input.isdigit():
         tab_index = int(user_input)
@@ -66,22 +72,30 @@ def closeTab():
         print("Invalid input, try again \n")
 
 
+# This function will display the HTML code of the website that the user specifies
 def switchTab():
-    user_input = input("Enter the index of the tab you want to display: \n")
+    user_input = None
+    if len(open_tabs) == 1:
+        user_input = input("There is only one tab open at index 0\ntype 0 to display it's HTML code: ")
+    else:
+        user_input = input(
+            f"there are {len(open_tabs)} open tabs what index you want to display it's HTML from 0 -> {len(open_tabs) - 1}:  ")
 
     if user_input.isdigit():
         tab_index = int(user_input)
 
         if 0 <= tab_index < len(open_tabs):
+            # url will be taken from the list open_tabs specifying the index and taking what is under "url"
             url = open_tabs[tab_index]["url"]
         else:
+            # If it was out of range then it will print the url of the last index
             print("Invalid index. Using the last index \n")
             url = open_tabs[-1]["url"]
+    # The user doesn't enter anything will still display last index
     elif user_input == "":
         url = open_tabs[-1]["url"]
     else:
-        print("Invalid input. Using the last index.")
-        print()
+        print("Invalid input. Using the last index.\n")
         url = open_tabs[-1]["url"]
 
     # I'm using the try except in this case if the user enters a https:// with a random string then it won't
@@ -103,11 +117,14 @@ def switchTab():
 # This function will display all the tabs that are open as well as all nested tabs that are present
 def displayAllTabs():
     print("Titles of all open tabs are: ")
-    for items in open_tabs:
-        print(items["title"])
+    # used enumerate here to keep track of the index of the title in the current item
+    for i, items in enumerate(open_tabs):
+        print(f"title {i + 1}: ", items["title"])
+        # if items that are in open tabs have the following string then it will print the following /t is used to
+        # indicate that it is a nested tab
         if "Nested Tab: " in items:
             nested = items['Nested Tab: ']['title']
-            print(f"\t{nested}")
+            print(f"\tNested Title: {nested}")
     print()
 
 
@@ -116,9 +133,9 @@ def displayAllTabs():
 
 # This will choose the index of a tab and make a nested tab inside of it
 def openNestedTab():
-    user_input = input("Enter index of tab:")
+    user_input = input("Enter index of tab: ")
 
-# This will make sure it is index same as the above
+    # This will make sure it is index same as the above
     if user_input.isdigit():
         tab_index = int(user_input)
         # created a new dictionary names nested tab that will store a new title and url
