a42cf86f6f2a7edaf08d927699b0f5ab906f584e
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-11-19 13:32:40 +0200

    Assignment 4 created the menu and the main

---
+++ Assignment4.py
@@ -0,0 +1,14 @@
+def menu():
+    print("1. Singly Linked List")
+    print("2. Check if Palindrome")
+    print("3. Priority Queue")
+    print("4. Evaluate an Infix Expression")
+    print("5. Graph")
+    print("6. Exit")
+
+
+def main():
+    menu()
+
+
+main()
