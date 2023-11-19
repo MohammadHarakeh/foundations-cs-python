343437084837a7eff81ef25b3a43c686526f9b77
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-11-19 14:08:51 +0200

    added displayNode with an exmaple that will be removed when i add addNode

--- Assignment4.py
+++ Assignment4.py
@@ -14,15 +14,47 @@ def singlyLinkedListMenu():
     print("d. Return to main menu")
 
 
+class Node:
+
+    def __init__(self, info):
+        self.info = info
+        self.next = None
+class singlyLinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def displayNodes(self):
+        current = self.head
+        while current != None:
+            print(current.info, end=" ")
+            current = current.next
+        print()
+
+
 def main():
     menu()
+    ll = singlyLinkedList()
+
+    node1 = Node(20)
+    node2 = Node(10)
+    node3 = Node(15)
+    node4 = Node(7)
+
+    ll.head = node1
+    node1.next = node2
+    node2.next = node3
+    node3.next = node4
+
 
     choice = input("\nEnter choice 1 -> 6: ")
-    sub_choice = input("\nEnter choice a -> d: ")
+
 
     if choice == "1":
         singlyLinkedListMenu()
-        if sub_choice = "a":
+        sub_choice = input("\nEnter choice a -> d: ")
+        if sub_choice == "b":
+            ll.displayNodes()
 
 
 
